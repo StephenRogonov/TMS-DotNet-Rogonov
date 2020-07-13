059fb538fc82956b243b31da23e98f1432767067
Stephen Rogonov <55544681+StephenRogonov@users.noreply.github.com>
2020-07-13 17:02:00 +0300

    feat: QueueApp added.

---
+++ src/PostOffice QueueApp/Counters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace PostOfficeQueueApp
+{
+    class Counters
+    {
+        private int _x = 1;
+
+        public Counters(int x)
+        {
+            _x = x;
+        }
+
+        public void Counter1()
+        {
+            while (_x <= 100)
+            {
+                Console.WriteLine($"Customer {_x} go to Counter 1.");
+                _x++;
+                Thread counter2 = new Thread(new ThreadStart(Counter2));
+                if (!counter2.IsAlive) counter2.Start();
+                Random time = new Random();
+                Thread.Sleep(time.Next(3000, 7000));
+            }
+        }
+
+        public void Counter2()
+        {
+            while (_x <= 100)
+            {
+                Console.WriteLine($"Customer {_x} go to Counter 2.");
+                _x++;
+                Thread counter3 = new Thread(new ThreadStart(Counter3));
+                if (!counter3.IsAlive) counter3.Start();
+                Random time = new Random();
+                Thread.Sleep(time.Next(3000, 7000));
+            }
+        }
+
+        public void Counter3()
+        {
+            while (_x <= 100)
+            {
+                Console.WriteLine($"Customer {_x} go to Counter 3.");
+                _x++;                
+                Random time = new Random();
+                Thread.Sleep(time.Next(3000, 7000));
+            }
+        }
+    }
+}
---
+++ src/PostOffice QueueApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace PostOfficeQueueApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counters counters = new Counters(1);
+            Thread counter1 = new Thread(new ThreadStart(counters.Counter1));            
+            counter1.Start();
+        }
+    }
+}
